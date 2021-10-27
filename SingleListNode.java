8ec7dddb95e005e8a3697d1a6d35b7cd36f7c188
amgs <amgs>
2021-10-27 17:12:11 +0100

    Task1 done.2

---
+++ SingleListNode.java
@@ -0,0 +1,14 @@
+public class SingleListNode {
+    public Object getElement() {
+
+    }
+    
+    public SingleListNode getNext() {
+
+    }
+
+    public void setNext(SingleListNode node) {
+
+    }
+
+}
