3ae45d0202aaef3fcc7c103e0db3df8a8728d741
amgs <amgs>
2021-10-27 20:31:39 +0100

    Add SinglyLinkedList stub.

---
+++ SinglyLinkedList.java
@@ -0,0 +1,47 @@
+public class SinglyLinkedList {
+    private SingleListNode head;
+    private SingleListNode tail;
+    private int numElements;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.numElements = 0;
+    }
+
+    public Object getFirst() {
+
+    }
+
+    public Object getLast() {
+
+    }
+
+    public Object get(int position) {
+
+    }
+
+    public void insertFirst(Object element) {
+
+    }
+
+    public void insertLast(Object element) {
+
+    }
+
+    public void insert(Object element, int position) {
+
+    }
+
+    public Object removeFirst() {
+
+    }
+
+    public Object removeLast() {
+
+    }
+
+    public Object remove(int position) {
+
+    }
+}
