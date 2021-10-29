61ecf75ea828d7dc05039811606359a504d13f82
André Sabino <asabino@smarta>
2021-10-29 14:56:12 +0100

    Add SLL stub.

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,38 @@
+public class SinglyLinkedList {
+    public Object getFirst() {
+
+    }
+
+    public Object getLast() {
+
+    }
+
+    public Object get(int position) {
+
+    }
+
+    public void insertFirst(Object element) {
+
+    }
+
+    public void insertLast(Object element) {
+
+    }
+
+    public void insert(Object element, int position) {
+
+    }
+
+    public Object removeFirst() {
+
+    }
+
+    public Object removeLast() {
+
+    }
+
+    public Object remove(int position) {
+
+    }
+
+}
