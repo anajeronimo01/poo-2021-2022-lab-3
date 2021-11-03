a23ef6755a390b801515fb299e63dc1360e253e8
André Sabino <asabino@smarta>
2021-11-03 18:18:28 +0000

    SLN 100% coverage.

--- tests/SingleListNodeTest.java
+++ tests/SingleListNodeTest.java
@@ -1,11 +1,30 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SingleListNodeTest {
+    private SingleListNode node;
+
+    @BeforeEach
+    void setUp() {
+        node = new SingleListNode("Teste", null);
+    }
+
     @Test
     void testGetElement(){
-        SingleListNode node = new SingleListNode("Teste", null);
         assertEquals("Teste", node.getElement());
+    }
+
+    @Test
+    void testGetNext() {
+        assertNull(node.getNext());
+    }
+
+    @Test
+    void testSetNext() {
+        SingleListNode otherNode = new SingleListNode("Other", null);
+        node.setNext(otherNode);
+        assertEquals(otherNode, node.getNext());
     }
 }
