d34efa9838822ca292f92deec9784fc0e88d4cc1
André Sabino <asabino@smarta>
2021-11-05 14:05:50 +0000

    Add SLL.

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -29,25 +29,81 @@ public class SinglyLinkedList {
         SingleListNode singleListNode = new SingleListNode(element, this.head);
         this.head = singleListNode;
         this.numElements++;
+        if(numElements == 1) {
+            tail = head;
+        }
     }
 
     public void insertLast(Object element) {
-
+        SingleListNode newNode = new SingleListNode(element, null);
+        if(numElements == 0) {
+            tail = newNode;
+            head = newNode;
+        }
+        else {
+            tail.setNext(newNode);
+        }
+        numElements++;
     }
 
     public void insert(Object element, int position) {
-
+        if(position == 0) {
+            insertFirst(element);
+        }
+        else if(position == numElements-1) {
+            insertLast(element);
+        }
+        else {
+            SingleListNode previousNode = head;
+            for(int i=0; i<position-1; i++) {
+                previousNode = previousNode.getNext();
+            }
+            SingleListNode newNode =
+                    new SingleListNode(element, previousNode.getNext());
+            previousNode.setNext(newNode);
+            numElements++;
+        }
     }
 
     public Object removeFirst() {
-        return null;
+        Object element = head.getElement();
+        head = head.getNext();
+        numElements--;
+        return element;
     }
 
     public Object removeLast() {
-        return null;
+        Object element = tail.getElement();
+        SingleListNode previousNode = head;
+        for(int i=0; i<numElements-2; i++) {
+            previousNode = previousNode.getNext();
+        }
+        previousNode.setNext(null);
+        tail = previousNode;
+        numElements--;
+        if(numElements == 0) {
+            head = null;
+            tail = null;
+        }
+        return element;
     }
 
     public Object remove(int position) {
-        return null;
+        if(position == 0) {
+            return removeFirst();
+        }
+        else if(position == numElements-1) {
+            return removeLast();
+        }
+        else {
+            SingleListNode previousNode = head;
+            for(int i=0; i<position-1; i++) {
+                previousNode = previousNode.getNext();
+            }
+            SingleListNode target = previousNode.getNext();
+            Object element = target.getElement();
+            previousNode.setNext(target.getNext());
+            return element;
+        }
     }
 }
